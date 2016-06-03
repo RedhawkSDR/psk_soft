222d938dc3fa55948efe937f450e171b65ec5cc5
Kipp Bowen <jkb@axiosengineering.com>
2016-06-03 13:12:47 -0400

    CA-411 Bump sub-minor version.

--- build.sh
+++ build.sh
@@ -24,9 +24,9 @@ if [ "$1" = "rpm" ]; then
     if [ -e rh.psk_soft.spec ]; then
         mydir=`dirname $0`
         tmpdir=`mktemp -d`
-        cp -r ${mydir} ${tmpdir}/rh.psk_soft-2.0.0
-        tar czf ${tmpdir}/rh.psk_soft-2.0.0.tar.gz --exclude=".svn" -C ${tmpdir} rh.psk_soft-2.0.0
-        rpmbuild -ta ${tmpdir}/rh.psk_soft-2.0.0.tar.gz
+        cp -r ${mydir} ${tmpdir}/rh.psk_soft-2.0.1
+        tar czf ${tmpdir}/rh.psk_soft-2.0.1.tar.gz --exclude=".svn" -C ${tmpdir} rh.psk_soft-2.0.1
+        rpmbuild -ta ${tmpdir}/rh.psk_soft-2.0.1.tar.gz
         rm -rf $tmpdir
     else
         echo "Missing RPM spec file in" `pwd`
--- rh.psk_soft.spec
+++ rh.psk_soft.spec
@@ -30,7 +30,7 @@ Prefix:         %{_prefix}
 %define _infodir       %{_prefix}/info
 
 Name:           rh.psk_soft
-Version:        2.0.0
+Version:        2.0.1
 Release:        1%{?dist}
 Summary:        Component %{name}
 
